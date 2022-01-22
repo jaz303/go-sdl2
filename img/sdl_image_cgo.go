ea66a517c9e96e370449318a59e0ded9c6965eb0
Jason Frame <jwf@jasonframe.co.uk>
2022-01-22 15:47:38 +0000

    update cgo config to with with nix

--- img/sdl_image_cgo.go
+++ img/sdl_image_cgo.go
@@ -2,7 +2,7 @@
 
 package img
 
-//#cgo linux freebsd darwin pkg-config: sdl2
+//#cgo linux freebsd darwin pkg-config: sdl2_image
 //#cgo linux freebsd darwin LDFLAGS: -lSDL2_image
 //#cgo windows LDFLAGS: -lSDL2 -lSDL2_image
 import "C"
--- mix/sdl_mixer_cgo.go
+++ mix/sdl_mixer_cgo.go
@@ -3,6 +3,6 @@
 package mix
 
 //#cgo windows LDFLAGS: -lSDL2 -lSDL2_mixer
-//#cgo linux freebsd darwin pkg-config: sdl2
+//#cgo linux freebsd darwin pkg-config: sdl2_mixer
 //#cgo linux freebsd darwin LDFLAGS: -lSDL2_mixer
 import "C"
